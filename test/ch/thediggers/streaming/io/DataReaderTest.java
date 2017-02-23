e01e4d97ee7ce6c77590bcf007670231bb0a6cbe
Thomas Schweizer <tschweizer@digger.ch>
2017-02-23 20:36:28 +0100

    Location of test data

--- test/ch/thediggers/streaming/io/DataReaderTest.java
+++ test/ch/thediggers/streaming/io/DataReaderTest.java
@@ -3,6 +3,8 @@ package ch.thediggers.streaming.io;
 import ch.thediggers.streaming.models.InputData;
 import org.junit.Test;
 
+import java.io.File;
+
 /**
  * Created by Thomsch on 23.02.2017.
  */
@@ -10,7 +12,7 @@ public class DataReaderTest {
     @Test
     public void readFile() throws Exception {
 
-        InputData inputData = DataReader.readFile("me_at_the_zoo.in");
+        InputData inputData = DataReader.readFile("data" + File.separator + "me_at_the_zoo.in");
 
         System.out.println(inputData.numVideos);
         System.out.println(inputData.numEndPoint);
